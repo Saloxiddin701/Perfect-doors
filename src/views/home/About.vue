d4f262f949e01b21448b9470427afa965024176d
Saloxiddin701 <abduraxmon.69list.ru@gmail.com>
2022-07-18 23:00:31 +0500

    fixed bugs

--- src/views/home/About.vue
+++ src/views/home/About.vue
@@ -9,21 +9,21 @@
         <!--left-->
         <div class="flex flex-col justify-between items-center p-10 bg-brand rounded-tr-[100px] rounded-bl-[100px]">
           <img class="w-12 h-12 p-2 bg-white rounded-full" src="@/assets/icons/file.svg" alt="">
-          <h1 class="text-white text-4xl">{{ $t('quotes') }}</h1>
+          <h1 class="text-white text-4xl text-center">{{ $t('quotes') }}</h1>
           <h3 class="w-60 text-center mt-6 text-white">{{ $t('easy_quotes') }}</h3>
         </div>
 
         <!--center-->
         <div class="flex flex-col justify-between items-center p-10 border-2 border-brand rounded-tl-[100px] rounded-br-[100px]">
           <img class="w-12 h-12 p-2 bg-gray rounded-full" src="@/assets/icons/business.svg" alt="">
-          <h1 class="text-brand text-4xl">{{ $t('quotes') }}</h1>
+          <h1 class="text-brand text-4xl text-center">{{ $t('quotes') }}</h1>
           <h3 class="w-60 text-center mt-6 text-brand">{{ $t('easy_quotes') }}</h3>
         </div>
 
         <!--right-->
         <div class="flex flex-col justify-between items-center p-10 border-2 border-brand rounded-tr-[100px] rounded-bl-[100px]">
           <img class="w-12 h-12 p-2 bg-gray rounded-full" src="@/assets/icons/person.svg" alt="">
-          <h1 class="text-brand text-4xl">{{ $t('quotes') }}</h1>
+          <h1 class="text-brand text-4xl text-center">{{ $t('quotes') }}</h1>
           <h3 class="w-60 text-center mt-6 text-brand">{{ $t('easy_quotes') }}</h3>
         </div>
       </div>
